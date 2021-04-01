d00c4ce46f507e2d03661620d542aea4809add0e
Bruno Fenerich <brunomunarettifenerich@gmail.com>
2021-03-31 23:32:15 -0300

    Jogo trilha, commit inicial

---
+++ trilha/src/main/java/com/fatec/es3/trilha/TrilhaApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.es3.trilha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaApplication.class, args);
+	}
+
+}
---
+++ trilha/src/test/java/com/fatec/es3/trilha/TrilhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.es3.trilha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
